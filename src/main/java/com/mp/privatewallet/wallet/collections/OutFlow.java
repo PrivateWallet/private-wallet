6566825426bf00348826dc00b4cc319fd83b2d37
Mateusz Staskiewicz <mateuszstaskiewicz@gmail.com>
2018-05-24 21:40:38 +0200

    Refactoring

--- src/main/java/com/mp/privatewallet/wallet/collections/OutFlow.java
+++ src/main/java/com/mp/privatewallet/wallet/collections/OutFlow.java
@@ -3,13 +3,11 @@ package com.mp.privatewallet.wallet.collections;
 import com.mp.privatewallet.wallet.enums.Direction;
 import com.mp.privatewallet.wallet.enums.Periodicity;
 
-import java.math.BigDecimal;
-
 public class OutFlow extends CashFlow {
 
-    private static Direction DIRECTION = Direction.OUT;
+    private final static Direction DIRECTION = Direction.OUT;
 
-    public OutFlow(Double amount, Periodicity periodicity) {
+    public OutFlow(final Double amount, final Periodicity periodicity) {
         super(amount, periodicity, DIRECTION);
     }
 
