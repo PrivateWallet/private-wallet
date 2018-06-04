894f6244017cb1f5f1da3c109e2134369caec965
Staskiewicz, Mateusz <mateusz.staskiewicz@misys.com>
2018-06-04 15:23:48 +0200

    Updated tests

--- src/test/java/com/mp/privatewallet/wallet/period/PeriodComparatorTest.java
+++ src/test/java/com/mp/privatewallet/wallet/period/PeriodComparatorTest.java
@@ -1,6 +1,7 @@
 package com.mp.privatewallet.wallet.period;
 
 import com.mp.privatewallet.wallet.period.exceptions.PeriodComparatorException;
+import com.mp.privatewallet.wallet.period.exceptions.WrongPeriodException;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.Assert.*;
@@ -9,15 +10,14 @@ import java.time.LocalDate;
 @SpringBootTest
 public class PeriodComparatorTest {
 
-//TODO: method names should be more clearly
     @Test
-    public void getUpdatedPeriodTest() {
+    public void periodComparatorShouldReturnCommonPartOfPeriods() {
         //given
         final Comparable cashFlowPeriod = new Period(LocalDate.now(), LocalDate.now().plusYears(1));
         final Comparable customPeriod = new Period(LocalDate.now().plusMonths(2), LocalDate.now().plusMonths(5));
 
         //when
-        final Comparable updatedPeriod = PeriodComparator.getUpdatedPeriod(cashFlowPeriod, customPeriod);
+        final Comparable updatedPeriod = PeriodComparator.getCommonPartOfPeriods(cashFlowPeriod, customPeriod);
 
         //then
         assertEquals(updatedPeriod.getFrom(), customPeriod.getFrom());
@@ -25,13 +25,13 @@ public class PeriodComparatorTest {
     }
 
     @Test
-    public void getUpdatedPeriodTest2() {
+    public void periodComparatorShouldReturnCommonPartOfPeriods2() {
         //given
         final Comparable cashFlowPeriod = new Period(LocalDate.now(), LocalDate.now().plusMonths(5));
         final Comparable customPeriod = new Period(LocalDate.now().plusMonths(2), LocalDate.now().plusYears(1));
 
         //when
-        final Comparable updatedPeriod = PeriodComparator.getUpdatedPeriod(cashFlowPeriod, customPeriod);
+        final Comparable updatedPeriod = PeriodComparator.getCommonPartOfPeriods(cashFlowPeriod, customPeriod);
 
         //then
         assertEquals(updatedPeriod.getFrom(), customPeriod.getFrom());
@@ -39,13 +39,13 @@ public class PeriodComparatorTest {
     }
 
     @Test
-    public void getUpdatedPeriodTest3() {
+    public void periodComparatorShouldReturnCommonPartOfPeriods3() {
         //given
         final Comparable cashFlowPeriod = new Period(LocalDate.now().plusMonths(2), LocalDate.now().plusYears(1));
         final Comparable customPeriod = new Period(LocalDate.now(), LocalDate.now().plusMonths(5));
 
         //when
-        final Comparable updatedPeriod = PeriodComparator.getUpdatedPeriod(cashFlowPeriod, customPeriod);
+        final Comparable updatedPeriod = PeriodComparator.getCommonPartOfPeriods(cashFlowPeriod, customPeriod);
 
         //then
         assertEquals(updatedPeriod.getFrom(), cashFlowPeriod.getFrom());
@@ -53,37 +53,42 @@ public class PeriodComparatorTest {
     }
 
     @Test
-    public void getUpdatedPeriodTest4() {
+    public void periodComparatorShouldReturnCommonPartOfPeriods4() {
         //given
         final Comparable cashFlowPeriod = new Period(LocalDate.now().plusMonths(2), LocalDate.now().plusMonths(5));
         final Comparable customPeriod = new Period(LocalDate.now(), LocalDate.now().plusYears(1));
 
         //when
-        final Comparable updatedPeriod = PeriodComparator.getUpdatedPeriod(cashFlowPeriod, customPeriod);
+        final Comparable updatedPeriod = PeriodComparator.getCommonPartOfPeriods(cashFlowPeriod, customPeriod);
 
         //then
         assertEquals(updatedPeriod.getFrom(), cashFlowPeriod.getFrom());
         assertEquals(updatedPeriod.getTo(), cashFlowPeriod.getTo());
     }
 
-    @Test(expected = RuntimeException.class)
-    public void getUpdatedPeriodTestShouldThrowException() {
+    @Test(expected = PeriodComparatorException.class)
+    public void shouldThrowExceptionWhenPeriodsDontHaveCommonPart() {
         //given
         final Comparable cashFlowPeriod = new Period(LocalDate.now().plusMonths(2), LocalDate.now().plusMonths(5));
         final Comparable customPeriod = new Period(LocalDate.now().plusYears(1), LocalDate.now().plusYears(2));
 
         //when
-        final Comparable updatedPeriod = PeriodComparator.getUpdatedPeriod(cashFlowPeriod, customPeriod);
+        PeriodComparator.getCommonPartOfPeriods(cashFlowPeriod, customPeriod);
     }
 
     @Test(expected = PeriodComparatorException.class)
-    public void getUpdatedPeriodTestShouldThrowException2() {
+    public void shouldThrowExceptionWhenPeriodsDontHaveCommonPart2() {
         //given
         final Comparable cashFlowPeriod = new Period(LocalDate.now().plusYears(1), LocalDate.now().plusYears(2));
         final Comparable customPeriod = new Period(LocalDate.now().plusMonths(2), LocalDate.now().plusMonths(5));
 
         //when
-        final Comparable updatedPeriod = PeriodComparator.getUpdatedPeriod(cashFlowPeriod, customPeriod);
+        PeriodComparator.getCommonPartOfPeriods(cashFlowPeriod, customPeriod);
+    }
+
+    @Test(expected = WrongPeriodException.class)
+    public void shouldThrowExceptionWhenFromIsLaterThanTo() {
+        final Comparable period = new Period(LocalDate.now().plusYears(1), LocalDate.now());
     }
 
 }
